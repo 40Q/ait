75a9f566f104e4aef20666585c4ac5341bb28f1c
Maximo Martinez Soria <mmartinez@40q.agency>
2026-04-22 16:37:06 -0300

    feat: add redirects for ait-omega.vercel.app to pickup.ait-co.com

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "ait-omega.vercel.app" }],
+        destination: "https://pickup.ait-co.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
   async headers() {
     return [
       {
